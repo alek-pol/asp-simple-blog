7c248c49aaa34df2ad25743701b2819f09a625ec
alek_pol <a.polyanskiy@outlook.com>
2022-04-16 00:34:25 +0200

    make it better

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
 20.times do
   Post.create(
     title: Faker::Lorem.sentence,
